1c1a862eef8bc24acfc8c7926fa3e93449a76e6a
Kristoffer Dahl <kristoffer.dahl@poweroffice.no>
2025-11-14 15:57:56 +0100

    Add React-based Swagger UI documentation with Vite setup and workflow enhancements

--- openapi/swagger-initializer.js
+++ openapi/swagger-initializer.js
@@ -1,7 +1,4 @@
 window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: window.location.origin + '/openapi/swagger.json',
     dom_id: '#swagger-ui',
@@ -11,10 +8,9 @@ window.onload = function() {
       SwaggerUIStandalonePreset
     ],
     plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
+      
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    supportedSubmitMethods: []
   });
-
-  //</editor-fold>
 };
