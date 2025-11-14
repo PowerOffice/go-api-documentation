6da41ff338cce16e77d0e7920ceb664b74ded9c5
Kristoffer Dahl <kristoffer.dahl@poweroffice.no>
2025-11-14 15:21:27 +0100

    Refactor code structure for improved readability and maintainability

--- openapi/swagger-initializer.js
+++ openapi/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://blue-mud-080cd9503.6.azurestaticapps.net/openapi/swagger.json",
+    url: window.location.origin + '/openapi/swagger.json',
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
